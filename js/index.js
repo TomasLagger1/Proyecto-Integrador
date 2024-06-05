5356e1a12ea999d64ac593cd86615a008f26aaa5
TomasLagger1 <tlagger@udesa.edu.ar>
2024-06-04 22:14:37 -0300

    js basico en indexjs

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
 let testeo1 = "hola"
 
 console.log(testeo1)
+
+// fetch('https://fakestoreapi.com/products')
+//  .then(function(res){
+//     return res.json()
+//  })
+
+//  .then(function (data) {
+//     console.log(data);
+//     let seccion = document.querySelector(".kection")
+//     let products = ""
+//     for (let i = 0; i < data.results.length; i++){
+//         products = products + `<article>
+//                               <a href = "./detalle.html?id=${data.results[i].id}">  
+//                               <img src=${data.results[i].image} >
+//                               </a>
+//                               <h2>${data.results[i].name}<h2>
+//                            </article>`
+//     }
+//     seccion.innerHTML = products
+
+//  })
+
+//  .catch(function (err){
+//    console.log(err);
+// })
