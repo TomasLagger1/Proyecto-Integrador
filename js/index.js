701125a0d0ccf2cd1982ea736af0a7619e199c82
Pedronadi <pnadinic@gmail.com>
2024-06-12 17:09:40 -0300

    Hice la ulitma seccion, la de las joyas

--- js/index.js
+++ js/index.js
@@ -86,3 +86,40 @@
 
 
 // INDEX JEWERLY SECTION
+
+let joyas= document.querySelector(".contenedoresIndextres")
+let Item= ;
+
+fetch('https://fakestoreapi.com/products/category/jewelery')
+    .then (function(response){
+        return response.json()
+    })
+
+    .then (function(data){
+        console.log(data);
+        for (let i=0; i<data.length; i++){
+            let stock= data [i];
+            item=`
+            <article class="productosIndex">
+            <div>
+            <h3 class="pipipi">${stock.title}</h3>
+            <img class="imgIndex" src="${stock.image}">
+            <ul class="listasIndex">
+            <li class="precioIndexJs">$${stock.price}</li>
+            </ul>
+            </div>
+             <div>
+               <button class="botonIndex" type="submit"> <a href="./cart.html">Agregar al carrito</a> </button>
+            </div>
+            <div>
+               <button class="botonIndex" type="submit"> <a href="./producto.html?id=${stock.id}">Ver más</a> </button>
+            </div>
+            </section>
+            </article>
+            `
+            joyas.innerHTML += item;
+        }
+    })
+    .catch (function(err){
+        console.log(err);
+    })
