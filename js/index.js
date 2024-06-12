4ef7f4b4dce2896b6fa457b5b28b904833192530
TomasLagger1 <tlagger@udesa.edu.ar>
2024-06-11 21:17:59 -0300

    seccion hombres del index masomenos hecha

--- js/index.js
+++ js/index.js
@@ -1,28 +1,36 @@
-let testeo1 = "hola"
+// let testeo1 = "hola"
 
-console.log(testeo1)
+// console.log(testeo1)
 
-// fetch('https://fakestoreapi.com/products')
-//  .then(function(res){
-//     return res.json()
-//  })
 
-//  .then(function (data) {
-//     console.log(data);
-//     let seccion = document.querySelector(".kection")
-//     let products = ""
-//     for (let i = 0; i < data.results.length; i++){
-//         products = products + `<article>
-//                               <a href = "./detalle.html?id=${data.results[i].id}">  
-//                               <img src=${data.results[i].image} >
-//                               </a>
-//                               <h2>${data.results[i].name}<h2>
-//                            </article>`
-//     }
-//     seccion.innerHTML = products
+// INDEX HOMBRES SECTION
 
-//  })
+   let hombress= document.querySelector(".contenedoresIndexuno")
+   let item="";
 
-//  .catch(function (err){
-//    console.log(err);
-// })
+   fetch('https://fakestoreapi.com/products/category/men\'s clothing')
+       .then (function(response){
+           return response.json()
+       })
+
+       .then (function(data){
+           console.log(data);
+           for (let i=0; i<data.length; i++){
+               let stock= data [i];
+               item=`
+               <h3>${stock.title}</h3>
+               <img class="imgIndex" src="${stock.image}">
+               <ul class="listasIndex">
+               <li>${stock.description}</li>
+               <li>$${stock.price}</li>
+               </ul>
+               <section class="botonIndex">
+               <a href="./producto.html?id=${stock.id}">VER MAS</a>
+               </section>
+               `
+               hombress.innerHTML += item;
+           }
+       })
+       .catch (function(err){
+           console.log(err);
+       })
