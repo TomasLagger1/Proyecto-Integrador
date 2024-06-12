0b7fdf3dce5c8b42ef23236a185e1ee0e59f9133
vmeggiolaro <vmeggiolaro@udesa.edu.ar>
2024-06-12 14:26:34 -0300

    hice la sección 2 del home

--- js/index.js
+++ js/index.js
@@ -41,3 +41,41 @@
        .catch (function(err){
            console.log(err);
        })
+
+
+       let muujeres= document.querySelector(".contenedoresIndexdos")
+       let Item=
+    
+       fetch('https://fakestoreapi.com/products/category/women\'s clothing')
+           .then (function(response){
+               return response.json()
+           })
+    
+           .then (function(data){
+               console.log(data);
+               for (let i=0; i<data.length; i++){
+                   let stock= data [i];
+                   item=`
+                   <article class="productosIndex">
+                   <div>
+                   <h3 class="pipipi">${stock.title}</h3>
+                   <img class="imgIndex" src="${stock.image}">
+                   <ul class="listasIndex">
+                   <li class="precioIndexJs">$${stock.price}</li>
+                   </ul>
+                   </div>
+                    <div>
+                      <button class="botonIndex" type="submit"> <a href="./cart.html">Agregar al carrito</a> </button>
+                   </div>
+                   <div>
+                      <button class="botonIndex" type="submit"> <a href="./producto.html?id=${stock.id}">Ver más</a> </button>
+                   </div>
+                   </section>
+                   </article>
+                   `
+                   muujeres.innerHTML += item;
+               }
+           })
+           .catch (function(err){
+               console.log(err);
+    })
