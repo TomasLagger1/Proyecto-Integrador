fc629ec714d8702fd331bad717f5a7422be3599b
TomasLagger1 <tlagger@udesa.edu.ar>
2024-06-24 14:07:41 -0300

    carrito finalizado

--- js/cart.js
+++ js/cart.js
@@ -1,42 +1,42 @@
-let idLocal = localStorage.getItem('cart');
-let idjason = JSON.parse(idLocal);
+    let idLocal = localStorage.getItem('cart');
+    let idjason = JSON.parse(idLocal);
 
-if (idLocal == null) {
-    let texto = document.querySelector("h5");
-    texto.innerHTML = "Su carrito está vacío";
-} else {
-    let carti = document.querySelector(".centu")
-    let carroo = ''
+    if (idLocal == null) {
+        let texto = document.querySelector("h5");
+        texto.innerHTML = "Su carrito está vacío";
+    } else {
+        let carti = document.querySelector (".centu")
+        let carroo = ''
 
-  idjason.forEach(id => {
-        fetch(`https://fakestoreapi.com/products/${id}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                let stock = data;
-                carroo += `
-                    <div>
-                        <div class="sprite"><img class="imagencarrocool" src="${stock.image}" alt="foto producto carrito"></div>
-                        <div class="spritecero">${stock.title}</div>
-                    </div>
-                `;
-                carti.innerHTML = carroo;
+        for (let i=0; i<idjason.length; i++){
+            fetch(`https://fakestoreapi.com/products/${idjason[i]}`)
+                .then (function(response){
+                return response.json()
+            })  
+            .then (function (stock){
+                console.log(stock);
+                carroo+= `
+                        <div>
+                            <div class="sprite"><img class="imagencarrocool" src="${stock.image}" alt="foto producto carrito"></div>
+                            <div class="spritecero">${stock.title}</div>
+                        </div>
+                    `;
+                    carti.innerHTML = carroo;
+                        })
+                    .catch (function(err){
+                        console.log(err);
                     })
-                .catch (function(err){
-                    console.log(err);
-                })
+
+        }
+    }
+    
+    var button = document.getElementById('botoncarro');
+
+    button.addEventListener('click', function() {
+        alert('Gracias por tu compra');
+        localStorage.clear();
 
     })
 
-}
 
-var button = document.getElementById('botoncarro');
-
-button.addEventListener('click', function() {
-    alert('Gracias por tu compra');
-    localStorage.clear();
-
-})
-
-
-console.log(idLocal) //si la respuesta de este es null, el h5 deberia poner que esta vacio y sino deberia quedar como estaba
+    console.log(idLocal) //si la respuesta de este es null, el h5 deberia poner que esta vacio y sino deberia quedar como estaba
